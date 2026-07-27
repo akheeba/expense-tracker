4b452db73c33f2f9a9e0abc7e8282dd296fc1f56
Akheeba <mohammedakheeba@gmail.com>
2026-07-27 08:57:26 +0530

    Update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://expense-tracker-server-s9te.onrender.com/api",
 });
 
 export default API;
